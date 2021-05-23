3d388ef58d75a8d6a34aa7f95e776d7e9451821a
MazenH24 <myr.2002@yahoo.com>
2021-05-24 01:51:16 +0300

    corrected some logical errors

--- larsen_inference.py
+++ larsen_inference.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 
 
 def calculate_membership(value, table_name):
+    """
+    :param value: crisp value
+    :param table_name: a table name indicating which value must be calculated
+    :return: membership value along with the linguistic variable
+    """
     table = {}
     if table_name == 'HV':
         table = data.market_value_of_houses_ms
@@ -38,6 +43,11 @@ def calculate_membership(value, table_name):
 
 
 def two_sets_cross_product(set_one, set_two):
+    """
+    :param set_one: membership value, linguistic variable pairs
+    :param set_two: membership value, linguistic variable pairs
+    :return: cartesian product of the sets
+    """
     combinations = []
     for subset_one in set_one:
         for subset_two in set_two:
@@ -46,6 +56,13 @@ def two_sets_cross_product(set_one, set_two):
 
 
 def four_sets_cross_product(set_one, set_two, set_three, set_four):
+    """
+    :param set_one: membership value, linguistic variable pairs
+    :param set_two: membership value, linguistic variable pairs
+    :param set_three: membership value, linguistic variable pairs
+    :param set_four: membership value, linguistic variable pairs
+    :return: cartesian product of the sets
+    """
     combinations = []
     for subset_one in set_one:
         for subset_two in set_two:
@@ -56,6 +73,11 @@ def four_sets_cross_product(set_one, set_two, set_three, set_four):
 
 
 def find_min(combinations, evaluation_table):
+    """
+    :param combinations: the combinations of the cross-products
+    :param evaluation_table: name indicating which rule set must be used
+    :return: linguistic variable, membership value pair extracted from rule set according to larsen inference method
+    """
     result = []
     if evaluation_table == 'HE':
         for combination in combinations:
@@ -86,6 +108,12 @@ def find_min(combinations, evaluation_table):
 
 
 def filter_memberships(memberships):
+    """
+    :param memberships: linguistic variable, membership value pair extracted from
+                        rule set according to larsen inference method
+    :return: if there is linguistic variable, membership value pairs with the same linguistic variable value only keep
+            the one with the max membership value
+    """
     new_memberships = []
     scanned = []
     for i in range(len(memberships)):
@@ -104,6 +132,12 @@ def filter_memberships(memberships):
 
 
 def larsen_inference_evaluation(memberships, table):
+    """
+    :param memberships: linguistic variable, membership value pair extracted from
+                        rule set according to larsen inference method
+    :param table: table name
+    :return: fuzzy number, membership pairs according to larsen inference method
+    """
     numbers = {}
     if table == 'HMS':
         numbers = data.house_ms
@@ -113,12 +147,17 @@ def larsen_inference_evaluation(memberships, table):
         numbers = data.credit_amount_ms
     triangular_numbers = []
     for membership in memberships:
-        triangular_numbers.append((numbers[membership[0]], membership[1]))
+        triangular_numbers.append((numbers[membership[0]], membership[1], membership[0]))
     triangular_numbers.sort(key=lambda x: x[0][3])
     return triangular_numbers
 
 
 def line_intersection(line1, line2):
+    """
+    :param line1: list of two points
+    :param line2: list of two points
+    :return: tuple (x, y) containing the intersection coordinates if the lines intersect
+    """
     x_diff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     y_diff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
@@ -135,14 +174,22 @@ def line_intersection(line1, line2):
     return x, y
 
 
-def union(numbers):
+def union(numbers, label):
+    """
+    :param numbers: fuzzy number, membership value pairs
+    :param label: name of the evaluation process
+    :return: fuzzy union of these fuzzy numbers method used is max()
+    """
+    plt.style.use('fivethirtyeight')
     x_points = []
     y_points = []
     for number in numbers:
         x_points.append([number[0][0], number[0][1], number[0][2], number[0][3]])
         y_points.append([0, number[1], number[1], 0])
     for i in range(len(x_points)):
-        plt.plot(x_points[i], y_points[i])
+        plt.plot(x_points[i], y_points[i], label=numbers[i][2])
+    plt.legend()
+    plt.title(label)
     plt.show()
     for i in range(len(x_points) - 1):
         line1 = ((x_points[i][2], y_points[i][2]), (x_points[i][3], y_points[i][3]))
@@ -164,29 +211,50 @@ def union(numbers):
             new_y_points.append(number)
     for i in range(len(new_x_points)):
         points_set.append((new_x_points[i], new_y_points[i]))
-    plt.plot(new_x_points, new_y_points)
+    plt.plot(new_x_points, new_y_points, label=label + ' Union')
+    plt.legend()
+    plt.title(label)
     plt.show()
 
     return points_set
 
 
 def find_line_from_2points(point1, point2):
+    """
+    :param point1: cartesian coordinate
+    :param point2: cartesian coordinate
+    :return: a list containing a line as follows [slope, y-intercept]
+    """
     slope = (point2[1] - point1[1]) / (point2[0] - point1[0])
     y_intercept = point2[1] - point2[0] * slope
     return [slope, y_intercept]
 
 
 def line_definite_integral(line, bounds):
+    """
+    :param line: [slope, y-intercept]
+    :param bounds: x-interval [x-left, x-right]
+    :return: definite integral of the line on the x-interval
+    """
     return (line[0] * (bounds[1] ** 2 / 2) + line[1] * bounds[1]) - (
             line[0] * (bounds[0] ** 2 / 2) + line[1] * bounds[0])
 
 
 def parabola_definite_integral(line, bounds):
+    """
+    :param line: [slope, y-intercept]
+    :param bounds: x-interval [x-left, x-right]
+    :return: definite integral of the line equation multiplied by 'x', on the x-interval
+    """
     return (line[0] * (bounds[1] ** 3 / 3) + line[1] * (bounds[1] ** 2 / 2)) - (
             line[0] * (bounds[0] ** 3 / 3) + line[1] * (bounds[0] ** 2 / 2))
 
 
 def center_of_area(points):
+    """
+    :param points: a list of cartesian points [(x1, y1), (x2, y2), ..., (xn, yn)]
+    :return: x value that identify the center of area formed by the list of points
+    """
     denominator = 0
     nominator = 0
     for i in range(len(points) - 1):
@@ -200,6 +268,13 @@ def center_of_area(points):
 
 
 def find_x(point_left, point_right, area, half_area):
+    """
+    :param point_left: a cartesian point in which half of the area lies after
+    :param point_right: a cartesian point in which half of the area lies before
+    :param area: summed subarea in which it's less than half of the area
+    :param half_area: the value of half of the area
+    :return: x-coordinate that split the curve into two equal areas
+    """
     line = find_line_from_2points(point_left, point_right)
     if line[0] != 0:
         coefficient = 2 / line[0]
@@ -215,10 +290,16 @@ def find_x(point_left, point_right, area, half_area):
         else:
             return x2
     else:
-        return (half_area - area) / line[1]
+        return (half_area - area + line[1]*point_left[0]) / line[1]
 
 
 def find_x_of_half_area(points, half_of_area):
+    """
+    a function that determines the x interval in which half of the area lies
+    :param points: a list of cartesian points [(x1, y1), (x2, y2), ..., (xn, yn)]
+    :param half_of_area: the value of half of the area
+    :return: x-coordinate that split the curve into two equal areas
+    """
     area = 0
     line_points = []
     for i in range(len(points) - 1):
@@ -230,11 +311,18 @@ def find_x_of_half_area(points, half_of_area):
         if area + sub_area > half_of_area:
             line_points = [points[i], points[i + 1]]
             break
+        elif area + sub_area == half_of_area:
+            return points[i][0]
         area += sub_area
     return find_x(line_points[0], line_points[1], area, half_of_area)
 
 
 def bisector_of_area(points):
+    """
+    determine half of the area value a list of cartesian points [(x1, y1), (x2, y2), ..., (xn, yn)]
+    :param points:
+    :return:
+    """
     total_area = 0
     for i in range(len(points) - 1):
         if ((points[i][0] == points[i + 1][0] and points[i][1] == points[i + 1][1]) or
@@ -246,22 +334,35 @@ def bisector_of_area(points):
     return find_x_of_half_area(points, half_of_area)
 
 
-def mean_of_maximum(points):
+def mean_of_maximum(points, defuzzified_table):
+    """
+    mean of maximum defuzzification method
+    :param points: a list of cartesian points [(x1, y1), (x2, y2), ..., (xn, yn)]
+    :param defuzzified_table: table name used for determining whether the fuzzy number is triangular or trapezoidal
+    :return: defuzzified value
+    """
+    tables_limit = {'H': 10, 'A': 10, 'C': 500}
     temp_points = points.copy()
     temp_points.sort(key=lambda x: x[1], reverse=True)
-    max_value = temp_points[0][1]
-    result = []
-    for point in temp_points:
-        if point[1] == max_value:
-            result.append(point[0])
-        else:
-            break
+    result = [temp_points[0][0], temp_points[1][0]]
+    if sum(result) / 2 > tables_limit[defuzzified_table]:
+        return result[0]
     return sum(result) / 2
 
 
-def defuzzify(points, method):
+def defuzzify(points, method, defuzzified_table):
+    """
+    wrapper
+    :param points: a list of cartesian points [(x1, y1), (x2, y2), ..., (xn, yn)]
+    :param method: a string indicating the defuzzification method
+    :param defuzzified_table: used for mean of max only
+    :return: defuzzified value
+    """
     methods = {'1': center_of_area, '2': bisector_of_area,
                '3': mean_of_maximum}
     defuzzification_method = methods[method]
-    defuzzified_value = defuzzification_method(points)
+    if method == '3':
+        defuzzified_value = defuzzification_method(points, defuzzified_table)
+    else:
+        defuzzified_value = defuzzification_method(points)
     return defuzzified_value
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import random
 from larsen_inference import calculate_membership
 from larsen_inference import filter_memberships
@@ -8,6 +7,63 @@ from larsen_inference import larsen_inference_evaluation
 from larsen_inference import two_sets_cross_product
 from larsen_inference import union
 from larsen_inference import defuzzify
+
+
+def print_report():
+    """
+    write the report of th operation in report.txt
+    """
+    with open('report.txt', 'w') as f:
+        f.write(f'---------------------------------------------------------------------------\n'
+                f'\nHouse Evaluation:\n'
+                f'\nThe House Market Value {inputs[0]} has the following memberships:\n '
+                f'{house_state_value}\n'
+                f'\nThe House Location Value {inputs[1]} has the following memberships:\n '
+                f'{house_state_location}\n'
+                f'\nBy taking the cross product of the memberships we obtain:\n'
+                f'{house_cross_product_set}\n'
+                f'\nAccording to the House Evaluation rule set The house evaluation has\n'
+                f'the following memberships:\n'
+                f'{house_filtered_cross_product}\n'
+                f'\nAfter defuzzification the house has the following value:\n'
+                f'{home_defuzzified_value}\n'
+                f'\n---------------------------------------------------------------------------'
+                f'\nApplicant Evaluation:\n'
+                f'\nThe Applicant Asset Value {inputs[2]} has the following memberships:\n '
+                f'{assets_state}\n'
+                f'\nThe Applicant Income Value {inputs[3]} has the following memberships:\n '
+                f'{income_state}\n'
+                f'\nBy taking the cross product of the memberships we obtain:\n'
+                f'{applicant_cross_product_set}\n'
+                f'\nAccording to the Applicant Evaluation rule set The applicant evaluation has\n'
+                f'the following memberships:\n'
+                f'{applicant_filtered_cross_product}\n'
+                f'\nAfter defuzzification the applicant has the following value:\n'
+                f'{applicant_defuzzified_value}\n'
+                f'\n---------------------------------------------------------------------------'
+                f'\nCredit Evaluation:\n'
+                f'\nThe Interest Value {inputs[4]} has the following memberships:\n '
+                f'{interest_state}\n'
+                f'\nThe House Defuzzified Value {inputs[5]} has the following memberships:\n '
+                f'{house_state_evaluation}\n'
+                f'\nThe Applicant Defuzzified Value {inputs[6]} has the following memberships:\n '
+                f'{applicant_state_evaluation}\n'
+                f'\nThe Applicant Income Value {inputs[3]} has the following memberships:\n '
+                f'{income_state}\n'
+                f'\nBy taking the cross product of the memberships we obtain:\n')
+        for subset in credit_cross_product_set:
+            f.write(f'{subset}\n')
+        f.write(f'\nAccording to the Credit Evaluation rule set The Credit evaluation has\n'
+                f'the following memberships:\n'
+                f'{credit_filtered_cross_product}\n'
+                f'\nAfter defuzzification the credit has the following value:\n'
+                f'{credit_defuzzified_value}\n'
+                f'\nThe credit amount that can be given is: '
+                f'{credit_defuzzified_value * 1000}$\n'
+                f'\n---------------------------------------------------------------------------'
+                )
+        print('\n\nthe program executed successfully you can check the report!')
+
 
 if __name__ == '__main__':
     # taking inputs
@@ -49,52 +105,47 @@ if __name__ == '__main__':
             false_input = False
         except AssertionError:
             print('Please check your inputs!')
-
     while stress_testing:
         if is_stress_testing == 'y':
-            inputs = [random.random()*1000, random.random()*10, random.random()*1000,
-                      random.random()*100, random.random()*10]
+            inputs = [random.random() * 1000, random.random() * 10, random.random() * 1000,
+                      random.random() * 100, random.random() * 10]
             defuzzification_method = str(random.randint(1, 3))
+            print(f'Inputs are {inputs}')
+            print(f'Defuzzification Method is {defuzzification_method}\n')
         elif is_stress_testing == 'n':
             stress_testing = False
         # house evaluation
         house_state_value = calculate_membership(inputs[0], 'HV')
-        # print(house_state_value)
         house_state_location = calculate_membership(inputs[1], 'HL')
-        # print(house_state_location)
-        cross_product_set = two_sets_cross_product(house_state_value, house_state_location)
-        # print(cross_product_set)
-        mapped_cross_product = find_min(cross_product_set, 'HE')
-        # print(mapped_cross_product)
-        filtered_cross_product = filter_memberships(mapped_cross_product)
-        # print(filtered_cross_product)
-        home_evaluation_results = larsen_inference_evaluation(filtered_cross_product, 'HMS')
-        # print(home_evaluation_results)
-        home_to_be_deffuzified = union(home_evaluation_results)
-        inputs.append(defuzzify(home_to_be_deffuzified, method=defuzzification_method))
-        print(defuzzify(home_to_be_deffuzified, method=defuzzification_method))
+        house_cross_product_set = two_sets_cross_product(house_state_value, house_state_location)
+        house_mapped_cross_product = find_min(house_cross_product_set, 'HE')
+        house_filtered_cross_product = filter_memberships(house_mapped_cross_product)
+        home_evaluation_results = larsen_inference_evaluation(house_filtered_cross_product, 'HMS')
+        home_to_be_deffuzified = union(home_evaluation_results, label='House Evaluation')
+        home_defuzzified_value = defuzzify(home_to_be_deffuzified, method=defuzzification_method,
+                                           defuzzified_table='H')
+        inputs.append(home_defuzzified_value)
         # applicant evaluation
         assets_state = calculate_membership(inputs[2], 'AA')
-        # print(assets_state)
         income_state = calculate_membership(inputs[3], 'IA')
-        # print(income_state)
-        cross_product_set = two_sets_cross_product(assets_state, income_state)
-        # print(cross_product_set)
-        mapped_cross_product = find_min(cross_product_set, 'AE')
-        # print(mapped_cross_product)
-        filtered_cross_product = filter_memberships(mapped_cross_product)
-        applicant_evaluation_results = larsen_inference_evaluation(filtered_cross_product, 'AMS')
-        applicant_to_be_deffuzified = union(applicant_evaluation_results)
-        inputs.append(defuzzify(applicant_to_be_deffuzified, method=defuzzification_method))
-        print(defuzzify(applicant_to_be_deffuzified, method=defuzzification_method))
+        applicant_cross_product_set = two_sets_cross_product(assets_state, income_state)
+        applicant_mapped_cross_product = find_min(applicant_cross_product_set, 'AE')
+        applicant_filtered_cross_product = filter_memberships(applicant_mapped_cross_product)
+        applicant_evaluation_results = larsen_inference_evaluation(applicant_filtered_cross_product, 'AMS')
+        applicant_to_be_deffuzified = union(applicant_evaluation_results, label='Applicant Evaluation')
+        applicant_defuzzified_value = defuzzify(applicant_to_be_deffuzified, method=defuzzification_method,
+                                                defuzzified_table='A')
+        inputs.append(applicant_defuzzified_value)
         # credit evaluation
         interest_state = calculate_membership(inputs[4], 'I')
         house_state_evaluation = calculate_membership(inputs[5], 'HE')
         applicant_state_evaluation = calculate_membership(inputs[6], 'AE')
-        cross_product_set = four_sets_cross_product(interest_state, house_state_evaluation,
-                                                    applicant_state_evaluation, income_state)
-        mapped_cross_product = find_min(cross_product_set, 'CE')
-        filtered_cross_product = filter_memberships(mapped_cross_product)
-        credit_evaluation_result = larsen_inference_evaluation(filtered_cross_product, 'CMS')
-        to_be_deffuzified = union(credit_evaluation_result)
-        print(defuzzify(to_be_deffuzified, method=defuzzification_method))
+        credit_cross_product_set = four_sets_cross_product(interest_state, house_state_evaluation,
+                                                           applicant_state_evaluation, income_state)
+        credit_mapped_cross_product = find_min(credit_cross_product_set, 'CE')
+        credit_filtered_cross_product = filter_memberships(credit_mapped_cross_product)
+        credit_evaluation_result = larsen_inference_evaluation(credit_filtered_cross_product, 'CMS')
+        to_be_deffuzified = union(credit_evaluation_result, label='Credit Evaluation')
+        credit_defuzzified_value = defuzzify(to_be_deffuzified, method=defuzzification_method, defuzzified_table='C')
+        if not stress_testing:
+            print_report()
